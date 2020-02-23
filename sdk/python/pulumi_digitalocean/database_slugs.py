6d0ecaafa6f520c4279544f72e6090e673d82812
rienafairefr <rienafairefr@gmail.com>
2020-02-23 12:45:55 +0100

    some constants

---
+++ sdk/python/pulumi_digitalocean/database_slugs.py
@@ -0,0 +1,7 @@
+DB_1VPCU1GB = "db-s-1vcpu-1gb"
+DB_1VPCU2GB = "db-s-1vcpu-2gb"
+DB_2VPCU4GB = "db-s-2vcpu-4gb"
+DB_4VPCU8GB = "db-s-4vcpu-8gb"
+DB_6VPCU16GB = "db-s-6vcpu-16gb"
+DB_8VPCU32GB = "db-s-8vcpu-32gb"
+DB_16VPCU64GB = "db-s-16vcpu-64gb"
